66cdcf134400beb7f1c0cbcff07e0af2f0ba4cf2
Ubuntu <ubuntu@ip-172-31-2-250.ap-south-1.compute.internal>
2025-05-05 06:15:51 +0000

    Fixed And Tested

--- app.py
+++ app.py
@@ -18,4 +18,5 @@ def greet_path(name):
     return render_template("greet.html", name=name, title="Greeting")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
